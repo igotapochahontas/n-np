11dee5d4e6d9507b2036ddd0d3bb4fd3709eb269
igotapochahontas <igotapochahontas@yahoo.com>
2019-04-22 09:07:00 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,15 @@
-// let v = "hello world";
+ let v = "hello world";
 function setup() {
   createCanvas(800, 1200);
 
- // textSize(100);
- // textAlign(CENTER, CENTER);
-}
+  textSize(100);
+  textAlign(CENTER, CENTER);
+
 
 }
 
 function draw() {
   background(245);
 
-// text(v,0,1000);
+ text(v,0,0);
 }
