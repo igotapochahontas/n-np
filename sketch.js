922b13f855bf8a3b0e418cf36116aa4b28559bbe
igotapochahontas <igotapochahontas@yahoo.com>
2019-04-28 10:57:55 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
-var input; 
+var fartBox; 
 var v = "C:< hello world!";
 function setup() {
   createCanvas(800, 1200);
-input = createInput("Ram your maths in here");
-input.changed(updateText);
+fartBox = createInput("Ram your maths in here");
+fartBox.changed(updateText);
 }
 function updateText(){
 v = 0;
-v = input.value();
+v = fartBox.value();
 }
 function draw() {
   background(0);
