2de1fdb1284da47a30592a333a7370b568015311
igotapochahontas <igotapochahontas@yahoo.com>
2019-05-12 19:07:25 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,9 +16,9 @@ textAlign(LEFT, BOTTOM);
 fill(0,255,0);
 text(v,0,1100);
 }
-Function math xv (+)(-)1 (-)(+)2 (+)(-)3(+)(-)4 if <0 then invalid
-Function math yv (+)(-)1 (-)(+)2 (+)(-)3(+)(-)4 if <0 then invalid
-Function store xv and yv
-Function 2 math if y is>4 then (-) 4 
-Function 2 math if y is>4 then (-)4
-Function use 2math if xv and yv are = to math vx and yv then statement is false.
+//Function math xv (+)(-)1 (-)(+)2 (+)(-)3(+)(-)4 if <0 then invalid
+//Function math yv (+)(-)1 (-)(+)2 (+)(-)3(+)(-)4 if <0 then invalid
+//Function store xv and yv
+//Function 2 math if y is>4 then (-) 4 
+//Function 2 math if y is>4 then (-)4
+//Function use 2math if xv and yv are = to math vx and yv then statement is false.
