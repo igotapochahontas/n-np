3a523106ffc64aff77c2c01701fcab5b37e5982b
igotapochahontas <igotapochahontas@yahoo.com>
2020-01-26 13:38:44 -0600

    Update new.js
    One confliction complete

--- new.js
+++ new.js
@@ -1,11 +1,21 @@
+var output = [];
+var hipstu; 
+var initv; 
+var inity;
+var initv1; 
+var inity1; 
+var vtemx; 
 var fartBox; 
+var abo; 
 var fartBoy;
 var fartbsize;
 var bsize = 8;
 var bfract = bsize -4; 
+var xycombo = []; 
+var combhip; 
 var hipster;
 var v;
-var va;
+
 var realfont; 
 var xdim = [];
 var ydim = [];
@@ -32,100 +42,76 @@ fartBoy.changed(jiyyinpants);
 function jixxinpants (){
   v = fartBox.value();
   hipster = parseInt(v);
+xdim.indexOf(hipster) === -1 ? xdim.push(hipster) : alert("X already exists");
 
-xdim.splice(hipster-1,1,0);
-for (i = 1; i <= bfract; i++){
-  one = hipster + i;
-  one2 = hipster - i;
+createCanvas(800, 1200);
 
-if (one2 >  0){
-va.push(one2);
-
-if (one < bfract) {
-  va.push(one);
-  
-}
-}
-  }
-
- createCanvas(800, 1200);
-gridparse();
 }
 
 function jiyyinpants (){
   vy = fartBoy.value();
   hipstery = parseInt(vy);
-ydim.splice(hipstery-1,1,0);
-for (i = 1; i <= bfract; i++){
-  oney = hipstery + i;
-  one2y = hipstery - i;
-
-if (one2y > 0) {
- vya.push(one2y);
-
-if (oney < bfract) {
-  vya.push(oney);
- 
-}
-}
-  }
- createCanvas(800, 1200);
+ydim.indexOf(hipstery) === -1 ? ydim.push(hipstery) : alert("Y already exists");
+combhip = "" + hipster + hipstery; 
+// ^ forces integer to string for concat
+ xycombo.push(combhip);
+createCanvas(800, 1200);
 gridparse();
 }
 
 
 function gridparse (){
-var vtemy = fartBoy.value();
-var  initv= parseInt(vtemy);
-var vtemx = fartBox.value();
-var inity= parseInt(vtemx);
- //var initv = hipster;
-// var inity = hipstery;
-if (initv > 4){
 
-initv = initv - 4;
-alert(initv); 
-  //console.log(initv);
+if (xycombo.length>2){
+initv = xycombo[1];
+inity = xycombo[0];
+initv1 = parseInt(initv); 
+inity1 = parseInt(inity); 
+vtemx = initv1-inity1; 
+abo = Math.abs(vtemx);
+alert(abo); 
+lastest();
 }
-if (inity > 4){
-
-inity = inity - 4;
-alert(inity); 
- }
 }
 function jixxinboard (){
-v = fartBsize.value();
-  offs = parseInt(v);
+va = fartBsize.value();
+  offs = parseInt(va);
 
-for (i = 1; i <= offs; i++){
-  xdim.push(i);
-  ydim.push(i)
-}
 
-v = 0;
+va = 0;
 realfont = offs * 1.5;
 bfract = offs; 
   return realfont; 
 return bfract; 
 gridparse();
-// createCanvas(800, 1200);
+
   }
-  
+  function lastest(){
+var number = abo,
+    output = [],
+    sNumber = number.toString();
+
+for (var i = 0, len = sNumber.length; i < len; i += 1) {
+hipstu = parseInt(+sNumber.charAt(i));
+output.indexOf(hipstu) === -1 ? output.push(hipstu) : alert("Diagonal Conflict");
+
+ 
+}
+
+alert(output);
+vya = output.toString();
+
+}
 function draw() {
   background(0);
 textSize(width / realfont);
 textAlign(LEFT, BOTTOM);
 fill(0,255,0);
-text(va,0,1100);
-text("X: "+xdim,0,500);
-text("Y: "+ydim,0,700);
-text(vya,0,100);
+text("Board size: "+offs,0,300);
+text("X,Y: "+xycombo,0,500);
+text("Diagonal: "+vya,0,700);
+
  
 }
 
-//Function math jixxinpants (+)(-)1 (-)(+)2 (+)(-)3(+)(-)4 if <0 then invalid
-//Function math yv (+)(-)1 (-)(+)2 (+)(-)3(+)(-)4 if <0 then invalid
-//Function store xv and yv
-//Function 2 math if y is>4 then (-) 4 
-//Function 2 math if y is>4 then (-)4
-//Function use 2math if xv and yv are = to math vx and yv then statement is false.
+
